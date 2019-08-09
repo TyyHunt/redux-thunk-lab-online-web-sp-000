09ae365b37241c8057869f01ecf276f2ab45067a
Tyler Hunt <tyhunt04@gmail.com>
2019-08-09 20:15:29 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap';
+import CatList from './CatList';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from './actions/catActions.js'
 
-class App extends Component {   
+class App extends Component {  
+
+  componentDidMount() {
+    if (this.props.catPics.length === 0) {
+      this.props.actions.fetchCats()
+    }
+  } 
   
   render() {
     return (
