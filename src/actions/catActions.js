9488b79a9b7f1584b8cc93635ea198c1bb044aca
Tyler Hunt <tyhunt04@gmail.com>
2019-08-09 20:16:56 +0000

    Automatically backed up by Learn

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,8 +1,8 @@
 export function fetchCats() {
   return (dispatch) => {
-    dispatch({ type: 'LOADING_CATS' });   
-    return fetch('http://localhost:4000/db')		
+    dispatch({ type: 'LOADING_CATS' });
+    return fetch('http://localhost:4000/db')
       .then(response => { return response.json()})
-      .then(cats => { dispatch({ type: 'FETCH_CATS', payload: cats.images })});	
+      .then(cats => { dispatch({ type: 'FETCH_CATS', payload: cats.images })});
   };
 }
